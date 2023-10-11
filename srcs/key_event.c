a47d1a0a809ca39cfae40b00fb67a81fcec15aab
Hyungjun <pparkheong96@naver.com>
2023-10-11 19:36:18 +0900

    hyungjup: rotate는 됨

--- srcs/key_event.c
+++ srcs/key_event.c
@@ -7,7 +7,7 @@ int	key_press(int key_code, t_cub *cub)
 
 	if (key_code == KEY_W)
 	{
-		if (!cub->map[(int)(cub->pos.x + cub->dir.x * cub->move_speed)][(int)(1)])
+		if (!cub->map[(int)(cub->pos.x + cub->dir.x * cub->move_speed)][(int)(cub->pos.y)])
 			cub->pos.x += cub->dir.x * cub->move_speed;
 		if (!cub->map[(int)(cub->pos.x)][(int)(cub->pos.y + cub->dir.y * cub->move_speed)])
 			cub->pos.y += cub->dir.y * cub->move_speed;
@@ -16,7 +16,7 @@ int	key_press(int key_code, t_cub *cub)
 	{
 		old_dir_x = cub->dir.x;
 		cub->dir.x = cub->dir.x * cos(cub->rot_speed) - cub->dir.y * sin(cub->rot_speed);
-		cub->dir.y = old_dir_x * sin(cub->rot_speed) + cub->plane.y * cos(cub->rot_speed);
+		cub->dir.y = old_dir_x * sin(cub->rot_speed) + cub->dir.y * cos(cub->rot_speed);
 		old_plane_x = cub->plane.x;
 		cub->plane.x = cub->plane.x * cos(cub->rot_speed) - cub->plane.y * sin(cub->rot_speed);
 		cub->plane.y = old_plane_x * sin(cub->rot_speed) + cub->plane.y * cos(cub->rot_speed);
@@ -32,12 +32,14 @@ int	key_press(int key_code, t_cub *cub)
 	{
 		old_dir_x = cub->dir.x;
 		cub->dir.x = cub->dir.x * cos(-cub->rot_speed) - cub->dir.y * sin(-cub->rot_speed);
-		cub->dir.y = old_dir_x * sin(-cub->rot_speed) + cub->plane.y * cos(-cub->rot_speed);
+		cub->dir.y = old_dir_x * sin(-cub->rot_speed) + cub->dir.y * cos(-cub->rot_speed);
 		old_plane_x = cub->plane.x;
 		cub->plane.x = cub->plane.x * cos(-cub->rot_speed) - cub->plane.y * sin(-cub->rot_speed);
 		cub->plane.y = old_plane_x * sin(-cub->rot_speed) + cub->plane.y * cos(-cub->rot_speed);
 	}
-	if (key_code == KEY_ESC)
+	else if (key_code == KEY_ESC)
 		exit(0);
+	mlx_clear_window(cub->mlx, cub->win);
+	main_loop(cub);
 	return (0);
 }

33ec86e9f15b89f32f27bc4ed8a12997a722eb4f
Hyungjun <pparkheong96@naver.com>
2023-10-12 19:57:11 +0900

    hyungjup: key press 정상작동

--- srcs/key_event.c
+++ srcs/key_event.c
@@ -1,18 +1,39 @@
 #include "../cub3D.h"
 
-int	key_press(int key_code, t_cub *cub)
+// int	is_movable_place(t_cub *cub, int x, int y)
+// {
+// 	if(cub->map[x][y] == '0' || cub->map[x][y] == 'N' || \
+// 		cub->map[x][y] == 'S' || cub->map[x][y] == 'E' \
+// 			|| cub->map[x][y] == 'W')
+// 		return (TRUE);
+// 	else
+// 		return (FALSE);
+// }
+
+void	key_code_W_S(t_cub *cub, int key_code)
+{
+	if (key_code == KEY_W)
+	{
+		if (cub->map[(int)(cub->pos.x + cub->dir.x * cub->move_speed)][(int)(cub->pos.y)] != '1')
+			cub->pos.x += cub->dir.x * cub->move_speed;
+		if (cub->map[(int)(cub->pos.x)][(int)(cub->pos.y + cub->dir.y * cub->move_speed)] != '1')
+			cub->pos.y += cub->dir.y * cub->move_speed;
+	}
+	if (key_code == KEY_S)
+	{
+		if (cub->map[(int)(cub->pos.x - cub->dir.x * cub->move_speed)][(int)cub->pos.y] != '1')
+			cub->pos.x -= cub->dir.x * cub->move_speed;
+		if (cub->map[(int)cub->pos.x][(int)(cub->pos.y - cub->dir.y * cub->move_speed)] != '1')
+			cub->pos.y -= cub->dir.y * cub->move_speed;
+	}
+}
+
+void	key_code_A_D(t_cub *cub, int key_code)
 {
 	double	old_dir_x;
 	double	old_plane_x;
 
-	if (key_code == KEY_W)
-	{
-		if (!cub->map[(int)(cub->pos.x + cub->dir.x * cub->move_speed)][(int)(cub->pos.y)])
-			cub->pos.x += cub->dir.x * cub->move_speed;
-		if (!cub->map[(int)(cub->pos.x)][(int)(cub->pos.y + cub->dir.y * cub->move_speed)])
-			cub->pos.y += cub->dir.y * cub->move_speed;
-	}
-	else if (key_code == KEY_A)
+	if (key_code == KEY_A)
 	{
 		old_dir_x = cub->dir.x;
 		cub->dir.x = cub->dir.x * cos(cub->rot_speed) - cub->dir.y * sin(cub->rot_speed);
@@ -21,14 +42,7 @@ int	key_press(int key_code, t_cub *cub)
 		cub->plane.x = cub->plane.x * cos(cub->rot_speed) - cub->plane.y * sin(cub->rot_speed);
 		cub->plane.y = old_plane_x * sin(cub->rot_speed) + cub->plane.y * cos(cub->rot_speed);
 	}
-	else if (key_code == KEY_S)
-	{
-		if (!cub->map[(int)(cub->pos.x - cub->dir.x * cub->move_speed)][(int)cub->pos.y])
-			cub->pos.x -= cub->dir.x * cub->move_speed;
-		if (!cub->map[(int)cub->pos.x][(int)(cub->pos.y - cub->dir.y * cub->move_speed)])
-			cub->pos.y -= cub->dir.y * cub->move_speed;
-	}
-	else if (key_code == KEY_D)
+	if (key_code == KEY_D)
 	{
 		old_dir_x = cub->dir.x;
 		cub->dir.x = cub->dir.x * cos(-cub->rot_speed) - cub->dir.y * sin(-cub->rot_speed);
@@ -37,7 +51,13 @@ int	key_press(int key_code, t_cub *cub)
 		cub->plane.x = cub->plane.x * cos(-cub->rot_speed) - cub->plane.y * sin(-cub->rot_speed);
 		cub->plane.y = old_plane_x * sin(-cub->rot_speed) + cub->plane.y * cos(-cub->rot_speed);
 	}
-	else if (key_code == KEY_ESC)
+}
+
+int	key_press(int key_code, t_cub *cub)
+{
+	key_code_W_S(cub, key_code);
+	key_code_A_D(cub, key_code);
+	if (key_code == KEY_ESC)
 		exit(0);
 	mlx_clear_window(cub->mlx, cub->win);
 	main_loop(cub);

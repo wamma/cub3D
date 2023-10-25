21147cb4f239962b80fc59e13423901f549997fe
Hyungjun <pparkheong96@naver.com>
2023-10-25 22:29:00 +0900

    hyungjup, eoh: click esc 함수 구현...

--- main.c
+++ main.c
@@ -3,14 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eoh <eoh@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: hyungjup <hyungjup@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 14:27:28 by hyungjup          #+#    #+#             */
-/*   Updated: 2023/10/25 20:51:48 by eoh              ###   ########.fr       */
+/*   Updated: 2023/10/25 22:28:11 by hyungjup         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+int	click_esc(t_cub *cub)
+{
+	(void)cub;
+	exit(1);
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
@@ -25,7 +32,8 @@ int	main(int argc, char **argv)
 	init_s_cub(&cub, &info_map);
 	load_image(&cub);
 	mlx_loop_hook(cub.mlx, &main_loop, &cub);
-	mlx_hook(cub.win, X_EVENT_KEY_PRESS, 0, &key_press, &cub);
+	mlx_hook(cub.win, X_EVENT_KEY_PRESS, 1, &key_press, &cub);
+	mlx_hook(cub.win, 17, 0, &click_esc, &cub);
 	mlx_loop(cub.mlx);
 	return (0);
 }
